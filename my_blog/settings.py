34b02d3ce8508f762cf38a7ce9e65e931ca04186
gpprog <gpapoutzas@gmail.com>
2020-11-20 11:36:36 +0200

    My First Blog  app, first commit

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
